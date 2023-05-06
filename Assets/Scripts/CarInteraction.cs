0a5b183cde1c171e174b128953addaf96da15664
yuliia_ivanechko <yuliia.ivanechko@gameloft.com>
2023-05-06 14:47:14 +0300

    * basic car interaction script

---
+++ Assets/Scripts/CarInteraction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInteraction : MonoBehaviour
+{
+    public GameObject alpacaPlayer;
+    [SerializeField] ParticleSystem successParticles;
+    // Start is called before the first frame update
+    void onAwake()
+    {
+        alpacaPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.GameObject == alpacaPlayer && alpacaPlayer.input.Controls.Interact.WasPressedThisFrame)
+        {
+            
+        }
+    }
+}
