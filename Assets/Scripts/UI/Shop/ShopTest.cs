6dfbf47cfd89e24543e292a6fc926b6e1d8ce312
Bohdan Marchak <50302855+marchakboh@users.noreply.github.com>
2023-05-06 15:47:20 +0300

    Changed shop script

--- Assets/Scripts/UI/Shop/ShopTest.cs
+++ Assets/Scripts/UI/Shop/ShopTest.cs
@@ -7,25 +7,55 @@ public class ShopTest : MonoBehaviour
     [SerializeField]
     private Shop _shop;
 
+    private ControlsInput input;
+    private bool isRightPress = false;
+    private bool isLeftPress = false;
+
     private int startIndex = 1;
+
+    private void Awake()
+    {
+        input = new ControlsInput();
+    }
 
     private void Start()
     {
         _shop.SelectItemAt(startIndex);
+
+        input.ShopAction.MoveActionRight.performed += ctx => isRightPress = true;
+        input.ShopAction.MoveActionRight.canceled  += ctx => isRightPress = false;
+
+        input.ShopAction.MoveActionLeft.performed += ctx => isLeftPress = true;
+        input.ShopAction.MoveActionLeft.canceled  += ctx => isLeftPress = false;
     }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (isRightPress)
         {
             startIndex++;
             if (!_shop.SelectItemAt(startIndex))
-                startIndex--; ;
+                startIndex--;
+
+            isRightPress = false;
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (isLeftPress)
         {
             startIndex--;
             if (!_shop.SelectItemAt(startIndex))
                 startIndex++;
+            
+            isLeftPress = false;
         }
         //ToDo to player controller
       /*  else if(Input.GetKeyDown(KeyCode.E)){
